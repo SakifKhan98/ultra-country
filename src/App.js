e00ceb658b0fc42fc7f9f7d2c1aee6d8f7445203
SakifKhan98 <61116402+SakifKhan98@users.noreply.github.com>
2020-08-26 04:46:08 +0600

    ultra nation react practice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Country from "./components/Country/Country";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.eu/rest/v2/all")
+      .then((res) => res.json())
+      .then((data) => {
+        setCountries(data);
+        const names = data.map((country) => country.name);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleAddCountry = (country) => {
+    const newCart = [...cart, country];
+    setCart(newCart);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
+      <h1>Country Loaded: {countries.length}</h1>
+      <h4>Country Aded: {cart.length}</h4>
+      <Cart cart={cart}></Cart>
+      {countries.map((country) => (
+        <Country
+          country={country}
+          handleAddCountry={handleAddCountry}
+          key={country.alpha3Code}
+        ></Country>
+      ))}
+    </div>
+  );
+}
+
+export default App;
+
+{
+  /* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,9 +54,5 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
+      </header> */
 }
-
-export default App;
